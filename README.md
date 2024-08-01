503e5cccbfb5de25971fa3b4f2146e9ff92f0bac
jldec <jurgen@jldec.me>
2024-08-01 21:05:39 +0100

    try to stream plain text

---
+++ README.md
@@ -0,0 +1,4 @@
+Test browser buffering
+
+https://x.com/threepointone/status/1818965603836641768
+https://gist.github.com/threepointone/b473807287172c2bbe97726343f1f97b
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,41 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+// https://x.com/threepointone/status/1818965603836641768
+// https://gist.github.com/threepointone/b473807287172c2bbe97726343f1f97b
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+  async fetch(): Promise<Response> {
+    // a simple streaming response
+
+    const encoder = new TextEncoder();
+
+    return new Response(
+      new ReadableStream({
+        async pull(controller) {
+					controller.enqueue(encoder.encode(`123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 1234\n`));
+					controller.enqueue(encoder.encode(`123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 1234\n`));
+					controller.enqueue(encoder.encode(`123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 1234\n`));
+          for (let i = 0; i < 5; i++) {
+            controller.enqueue(encoder.encode(`Hello world 😎😎: ${i}\n`));
+            await sleep(1000);
+          }
+          controller.close();
+        },
+      }),
+      {
+        status: 200,
+        headers: {
+					"X-Content-Type-Options": "nosniff",  // nosniff avoids buffering in chrome
+          "content-type": "text/plain; charset=utf8", // charset avoids buffering on firefox
+          // "content-type": "text/x-unknown", // this will not,
+          
+          // some otehr explicit headers just in case
+          "content-encoding": "identity",
+          "transfer-encoding": "chunked",
+        },
+      }
+    );
+  },
+};
